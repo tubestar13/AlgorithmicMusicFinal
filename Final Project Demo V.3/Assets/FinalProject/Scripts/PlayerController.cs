30c1e479c80628dce6f630b1a19f575b3f7ad762
tubestar13 <36242804+tubestar13@users.noreply.github.com>
2020-06-02 06:04:05 -0700

    Update Movement. Add Generative Music. Add Stinger.

--- Final Project Demo V.3/Assets/FinalProject/Scripts/PlayerController.cs
+++ Final Project Demo V.3/Assets/FinalProject/Scripts/PlayerController.cs
@@ -1,30 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator anim;
-    private enum State {idle, walking, jumping, falling}
+    private enum State { idle, walking, jumping, falling }
     private State state = State.idle;
     private Collider2D coll;
     [SerializeField] private LayerMask ground;
 
+    private int count;
+
+    public Text countText;
+
+    // Instantiate the heavy plugin library
+    private Hv_final_AudioLib pd;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         coll = GetComponent<Collider2D>();
+
+        ground = LayerMask.GetMask("Ground");
+
+        pd = GetComponent<Hv_final_AudioLib>();
+
+        //start the noise
+        pd.SendEvent(Hv_final_AudioLib.Event.Startstop);
+
     }
 
-    private void Update()
+
+    private void FixedUpdate()
     {
-        float hDirection = Input.GetAxis("Horizontal");
+        float hDirection = Input.GetAxisRaw("Horizontal");
 
 
-        if(hDirection < 0)
+        if (hDirection < 0)
         {
             rb.velocity = new Vector2(-5, rb.velocity.y);
             transform.localScale = new Vector2(-1, 1);
@@ -38,19 +54,37 @@ public class PlayerController : MonoBehaviour
 
         else
         {
-        
+            rb.velocity = new Vector2(0, rb.velocity.y);
         }
 
-        if(Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
+        VelocityState();
+        anim.SetInteger("state", (int)state);
+    }
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
         {
             rb.velocity = new Vector2(rb.velocity.x, 10f);
             state = State.jumping;
         }
 
-        VelocityState();
-        anim.SetInteger("state", (int)state);
+    }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Collectable"))
+        {
+            other.gameObject.SetActive(false);
+            pd.SendEvent(Hv_final_AudioLib.Event.Stinger);
+            if (other.gameObject.name == "seq1gem")
+                pd.SetFloatParameter(Hv_final_AudioLib.Parameter.Selectseq, 1);
+            else if (other.gameObject.name == "seq2gem")
+                pd.SetFloatParameter(Hv_final_AudioLib.Parameter.Selectseq, 2);
+            else if (other.gameObject.name == "seq3gem")
+                pd.SetFloatParameter(Hv_final_AudioLib.Parameter.Selectseq, 3);
         }
+        
+    }
 
     private void VelocityState()
     {
